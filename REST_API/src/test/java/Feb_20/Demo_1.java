fd2c7727f869e55c794044170d1f893c0cbf4fbf
avina <avina@LAPTOP-FO2093R8>
2022-04-11 22:16:33 +0530

    My First Commit

---
+++ REST_API/src/test/java/Feb_20/Demo_1.java
@@ -0,0 +1,31 @@
+package Feb_20;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Demo_1 {
+
+	public static void main(String[] args) {
+		
+		RestAssured.baseURI="https://reqres.in/api/unknown/2";
+		
+		RequestSpecification httpRequest =RestAssured.given();
+				
+		Response resp =httpRequest.request(Method.GET);
+		
+		int a =resp.getStatusCode();
+		
+		System.out.println(a);
+		
+		String b =resp.getStatusLine();
+		System.out.println(b);
+
+		String c=resp.getBody().asString();
+		
+		System.out.println(c);
+		
+	}
+
+}  
